decab80f4c2d73133dce92f22323455fc79f7965
AgrawalKashish <96725005+AgrawalKashish@users.noreply.github.com>
2023-06-16 13:45:05 +0530

    first frontend push

---
+++ project/Api_frontend/my_app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!</h1>
+      <Hello name="Kashish" lastname="Agrawal"/>
+    </div>
+  );
+}
+
+export default App;
